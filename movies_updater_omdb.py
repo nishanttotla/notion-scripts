4ec3d877abb6f622dc7a9cd5696e40dc3f72f24d
Nishant Totla <nishanttotla@gmail.com>
2024-10-03 12:52:30 -0400

    Remove csv functions from OMDB updater

--- movies_updater_omdb.py
+++ movies_updater_omdb.py
@@ -1,4 +1,3 @@
-import csv
 import os
 import requests
 from notion_client import Client
@@ -30,19 +29,6 @@ def notion_database_query_all(notion: Client, database_id: str) -> dict:
       "has_more": has_more
   }
   return new_database
-
-def read_csv(path: str) -> dict:
-  """Read an IMDB export CSV file and create a dictionary keyed by movie title."""
-  imdb_dict = {}
-  with open(path, mode ='r') as file:    
-    csv_file = csv.DictReader(file)
-    # Title is at position 5 in IMDB export
-    for lines in csv_file:
-      if lines["Title"] in imdb_dict:
-        pprint("Found repeated key: "+ lines["Title"])
-      else:
-        imdb_dict[lines["Title"]] = lines
-  return imdb_dict
 
 def notion_row_add_genres(title: str, db_row: dict, imdb_dict: dict):
   """Takes a row from the Notion DB and returns an updated version of it by filling in genre from imdb_dict"""
@@ -78,16 +64,6 @@ notion = Client(auth=os.environ["NOTION_TOKEN"])
 # the integration. It's a two way sharing.
 
 full_db = notion_database_query_all(notion, os.environ["MOVIES_DB"])
-imdb_dict = read_csv("imdb_exports.csv")
-
-
-### Updating genres
-# for result in full_db["results"]:
-#   title = result["properties"]["Title"]["title"][0]["plain_text"]
-#   new_db_row = {}
-#   new_db_row = notion_row_add_genres(title, new_db_row, imdb_dict)
-#   pprint("Updated genres for: " + title)
-#   notion.pages.update(**{"page_id": result["id"], "properties": new_db_row["properties"]})
 
 res = omdb_get_entity("tt13802576")
 pprint(res)
