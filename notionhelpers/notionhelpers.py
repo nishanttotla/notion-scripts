a5808dc70e420469d573160964520fe3744f98bb
Nishant Totla <nishanttotla@gmail.com>
2024-10-12 00:50:30 -0400

    Remove concept of old and new properties in notion helpers

--- notionhelpers/notionhelpers.py
+++ notionhelpers/notionhelpers.py
@@ -16,6 +16,12 @@ class ColumnType(Enum):
   CHECKBOX = 7
 
 
+class NotionRowUpdateConfig(Enum):
+  UNKNOWN = 0
+  COMBINE = 1
+  REPLACE = 2
+
+
 def notion_database_query_all(notion: Client, database_id: str) -> dict:
   """Return all rows for the database."""
   data = notion.databases.query(database_id)
@@ -39,115 +45,107 @@ def notion_database_query_all(notion: Client, database_id: str) -> dict:
 
 
 @dataclass
-class NotionRowProperties():
+class NotionRow():
   row_id: str
-  updated_properties: dict
-  old_properties: dict
-  force_update: bool
+  properties: dict
+  commit_required: bool
 
-  def __init__(self,
-               row_id: str,
-               updated_properties: dict,
-               old_properties: dict,
-               force_update=False):
+  def __init__(self, row_id: str, properties: dict):
     self.row_id = row_id
-    self.updated_properties = updated_properties
-    self.old_properties = old_properties
-    self.force_update = force_update
-    if force_update:
-      pprint("force_update=True")
+    self.properties = properties
+    self.commit_required = False
 
-  # TODO: Add getter functions here so that getting the value of a field from
-  # a Notion row is as easy as setting it. Getting will be from on old_properties.
-
-  # TODO: Why is there an old_properties and a new_properties at all? Can just have
-  # a single dictionary for this.
-
-  def maybe_update_field(self, col_type: ColumnType, name: str, value):
+  def update_field(self, col_type: ColumnType, name: str, value):
     # TODO: For Python 3.10 and above, switch case statements can be used.
     # Validate input types and call the right update function.
     if col_type == ColumnType.UNKNOWN:
       raise ValueError("Type was not set for field: " + name)
     elif col_type == ColumnType.TEXT:
-      self.maybe_update_text_field_internal(name, value)
+      self.update_text_field_internal(name, value)
     elif col_type == ColumnType.DATE:
-      self.maybe_update_date_field_internal(name, value)
+      self.update_date_field_internal(name, value)
     elif col_type == ColumnType.NUMBER:
-      self.maybe_update_number_field_internal(name, value)
+      self.update_number_field_internal(name, value)
     elif col_type == ColumnType.SELECT:
-      self.maybe_update_select_field_internal(name, value)
+      self.update_select_field_internal(name, value)
     elif col_type == ColumnType.MULTI_SELECT:
-      self.maybe_update_multi_select_field_internal(name, value)
+      self.update_multi_select_field_internal(name, value)
     elif col_type == ColumnType.FILE:
-      self.maybe_update_file_field_internal(name, value)
+      self.update_file_field_internal(name, value)
     else:
       raise NotImplementedError("No implementation yet for type: " + type.name)
 
-  # The update functions will skip updating if the existing property is non-empty
-  # unless force_update is set.
-  def maybe_update_text_field_internal(self, name: str, value: str):
-    if (len(self.old_properties[name]["rich_text"])
-        != 0) & (not self.force_update):
-      pprint("Skipping update for non-empty field '" + name + "' for row_id: " +
-             self.row_id)
-      return
+  ############################## Getter Functions ##############################
 
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["rich_text"] = [{
+  def get_id(self):
+    return row_id
+
+  def get_properties(self):
+    return properties
+
+  ############################## Setter Functions ##############################
+  # The setter functions only update the data values. It is assumed that the
+  # row provided in the constructor is a properly formed Notion row i.e. it has
+  # the field types and ids already set. This means that we don't need to worry
+  # about the dictionaries for each property being empty - just the values may
+  # be empty.
+
+  def update_text_field_internal(self, name: str, value: str):
+    # TODO: Implement ability to append text instead of replacing it.
+    self.properties[name]["rich_text"] = [{
         "plain_text": value,
         "text": {
             "content": value
         }
     }]
+    self.commit_required = True
 
-  def maybe_update_date_field_internal(self, name: str, value: str):
-    if (self.old_properties[name]["date"] != None) & (not self.force_update):
-      pprint("Skipping update for non-empty field: " + name)
-      return
+  def update_date_field_internal(self, name: str, value: str):
+    if self.properties[name]["date"] == None:
+      self.properties[name]["date"] = {"start": value}
+      self.commit_required = True
+    elif self.properties[name]["date"]["start"] != value:
+      self.properties[name]["date"]["start"] = value
+      self.commit_required = True
+    else:
+      pprint("Update not required for field: " + name)
 
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["date"] = {"start": value}
+  def update_number_field_internal(self, name: str, value: int):
+    if self.properties[name]["number"] == None:
+      self.properties[name]["number"] = value
+      self.commit_required = True
+    elif self.properties[name]["number"] != value:
+      self.properties[name]["number"] = value
+      self.commit_required = True
+    else:
+      pprint("Update not required for field: " + name)
 
-  def maybe_update_number_field_internal(self, name: str, value: int):
-    if (self.old_properties[name]["number"] != None) & (not self.force_update):
-      pprint("Skipping update for non-empty field: " + name)
-      return
+  def update_select_field_internal(self, name: str, value: str):
+    if self.properties[name]["select"] == None:
+      self.properties[name]["select"] = {"name": value}
+      self.commit_required = True
+    elif self.properties[name]["select"]["name"] != value:
+      self.properties[name]["select"]["name"] = value
+      self.commit_required = True
+    else:
+      pprint("Update not required for field: " + name)
 
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["number"] = value
-
-  def maybe_update_select_field_internal(self, name: str, value: str):
-    if (self.old_properties[name]["select"] != None) & (not self.force_update):
-      pprint("Skipping update for non-empty field: " + name)
-      return
-
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["select"] = {"name": value}
-
-  def maybe_update_multi_select_field_internal(self, name: str, value: list):
-    if (len(self.old_properties[name]["multi_select"])
-        != 0) & (not self.force_update):
-      pprint("Skipping update for non-empty field: " + name)
-      return
-
+  def update_multi_select_field_internal(self, name: str, value: list):
     list_tagged = []
     for item in value:
       list_tagged.append({"name": item})
 
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["multi_select"] = list_tagged
+    # TODO: Implement ability to perform a union of the current and new lists
+    # and also figure out how to pass it in every function call
+    self.properties[name]["multi_select"] = list_tagged
+    self.commit_required = True
 
-  def maybe_update_file_field_internal(self, name: str, value: str):
-    if (len(self.old_properties[name]["files"]) != 0) & (not self.force_update):
-      pprint("Skipping update for non-empty field: " + name)
-      return
+  def update_file_field_internal(self, name: str, value: str):
+    # TODO: Implement ability to append file instead of replacing it.
 
-    # TODO: This is a break in abstraction, assuming that Title is a field. We
-    # need another way to fetch an identifier here. Probably pass it as an
-    # argument.
-    title = self.old_properties["Title"]["title"][0]["plain_text"]
-    self.updated_properties[name] = self.old_properties[name]
-    self.updated_properties[name]["files"] = [{
+    # TODO: Pass title as a parameter
+    title = "Unnamed file"
+    self.properties[name]["files"] = [{
         "external": {
             "url": value
         },
