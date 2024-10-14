12bdbb62f8a7872445b7f70357642e500b64debd
Nishant Totla <nishanttotla@gmail.com>
2024-10-14 14:06:05 -0400

    Update internal members of notionrowhelpers class to have __

--- notionhelpers/notionhelpers.py
+++ notionhelpers/notionhelpers.py
@@ -44,9 +44,9 @@ def notion_database_query_all(notion: Client, database_id: str) -> dict:
 
 @dataclass
 class NotionRow():
-  row_id: str
-  properties: dict
-  commit_required: bool
+  __row_id: str
+  __properties: dict
+  __commit_required: bool
   __pending_update: dict
 
   # TODO: Make these variables private
@@ -54,21 +54,21 @@ class NotionRow():
   ##### TODO: Add an update properties field here because you can't update the full row, that will cause auto fields needing to be updated.
 
   def __init__(self, row_id: str, properties: dict):
-    self.row_id = row_id
-    self.properties = properties
+    self.__row_id = row_id
+    self.__properties = properties
     self.__pending_update = {}
-    self.commit_required = False
+    self.__commit_required = False
 
   ############################## Getter Functions ##############################
 
   def get_id(self):
-    return self.row_id
+    return self.__row_id
 
   def get_pending_update(self):
     return self.__pending_update
 
   def is_commit_required(self):
-    return self.commit_required
+    return self.__commit_required
 
   ############################## Setter Functions ##############################
   # The setter functions only update the data values. It is assumed that the
@@ -87,94 +87,94 @@ class NotionRow():
     # TODO: For Python 3.10 and above, switch case statements can be used.
     # Validate input types and call the right update function.
     if col_type == ColumnType.TEXT:
-      self.update_text_field_internal(name, value, update_config)
+      self.__update_text_field_internal(name, value, update_config)
     elif col_type == ColumnType.DATE:
-      self.update_date_field_internal(name, value)
+      self.__update_date_field_internal(name, value)
     elif col_type == ColumnType.NUMBER:
-      self.update_number_field_internal(name, value)
+      self.__update_number_field_internal(name, value)
     elif col_type == ColumnType.SELECT:
-      self.update_select_field_internal(name, value)
+      self.__update_select_field_internal(name, value)
     elif col_type == ColumnType.MULTI_SELECT:
-      self.update_multi_select_field_internal(name, value, update_config)
+      self.__update_multi_select_field_internal(name, value, update_config)
     elif col_type == ColumnType.FILE:
-      self.update_file_field_internal(name, value, title, update_config)
+      self.__update_file_field_internal(name, value, title, update_config)
     else:
       raise NotImplementedError("No implementation yet for type: " + type.name)
 
-  def update_text_field_internal(self, name: str, value: str,
-                                 update_config: NotionRowUpdateConfig):
+  def __update_text_field_internal(self, name: str, value: str,
+                                   update_config: NotionRowUpdateConfig):
     # TODO: Implement ability to append text instead of replacing it.
-    self.properties[name]["rich_text"] = [{
+    self.__properties[name]["rich_text"] = [{
         "plain_text": value,
         "text": {
             "content": value
         }
     }]
-    self.commit_required = True
+    self.__commit_required = True
 
-  def update_date_field_internal(self, name: str, value: str):
-    if self.properties[name]["date"] == None:
-      self.properties[name]["date"] = {"start": value}
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
-    elif self.properties[name]["date"]["start"] != value:
-      self.properties[name]["date"]["start"] = value
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
+  def __update_date_field_internal(self, name: str, value: str):
+    if self.__properties[name]["date"] == None:
+      self.__properties[name]["date"] = {"start": value}
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
+    elif self.__properties[name]["date"]["start"] != value:
+      self.__properties[name]["date"]["start"] = value
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
     else:
       pprint("Update not required for field: " + name)
 
-  def update_number_field_internal(self, name: str, value: int):
-    if self.properties[name]["number"] == None:
-      self.properties[name]["number"] = value
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
-    elif self.properties[name]["number"] != value:
-      self.properties[name]["number"] = value
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
+  def __update_number_field_internal(self, name: str, value: int):
+    if self.__properties[name]["number"] == None:
+      self.__properties[name]["number"] = value
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
+    elif self.__properties[name]["number"] != value:
+      self.__properties[name]["number"] = value
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
     else:
       pprint("Update not required for field: " + name)
 
-  def update_select_field_internal(self, name: str, value: str):
-    if self.properties[name]["select"] == None:
-      self.properties[name]["select"] = {"name": value}
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
-    elif self.properties[name]["select"]["name"] != value:
-      self.properties[name]["select"]["name"] = value
-      self.__pending_update[name] = self.properties[name]
-      self.commit_required = True
+  def __update_select_field_internal(self, name: str, value: str):
+    if self.__properties[name]["select"] == None:
+      self.__properties[name]["select"] = {"name": value}
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
+    elif self.__properties[name]["select"]["name"] != value:
+      self.__properties[name]["select"]["name"] = value
+      self.__pending_update[name] = self.__properties[name]
+      self.__commit_required = True
     else:
       pprint("Update not required for field: " + name)
 
-  def update_multi_select_field_internal(self, name: str, value: list,
-                                         update_config: NotionRowUpdateConfig):
+  def __update_multi_select_field_internal(
+      self, name: str, value: list, update_config: NotionRowUpdateConfig):
     list_tagged = []
     for item in value:
       list_tagged.append({"name": item})
 
     # TODO: Implement ability to perform a union of the current and new lists
     # and also figure out how to pass it in every function call
-    self.properties[name]["multi_select"] = list_tagged
-    self.__pending_update[name] = self.properties[name]
-    self.commit_required = True
+    self.__properties[name]["multi_select"] = list_tagged
+    self.__pending_update[name] = self.__properties[name]
+    self.__commit_required = True
 
-  def update_file_field_internal(self, name: str, value: str, title: str,
-                                 update_config: NotionRowUpdateConfig):
+  def __update_file_field_internal(self, name: str, value: str, title: str,
+                                   update_config: NotionRowUpdateConfig):
     # TODO: Implement ability to append file instead of replacing it.
 
     if not title:
       title = "Unnamed file"
-    self.properties[name]["files"] = [{
+    self.__properties[name]["files"] = [{
         "external": {
             "url": value
         },
         "type": "external",
         "name": "Poster for " + title
     }]
-    self.__pending_update[name] = self.properties[name]
-    self.commit_required = True
+    self.__pending_update[name] = self.__properties[name]
+    self.__commit_required = True
 
   ############################# Clearing Functions #############################
 
@@ -182,40 +182,40 @@ class NotionRow():
     # TODO: For Python 3.10 and above, switch case statements can be used.
     # Validate input types and call the right update function.
     if col_type == ColumnType.TEXT:
-      self.clear_text_field_internal(name, value)
+      self.__clear_text_field_internal(name, value)
     elif col_type == ColumnType.DATE:
-      self.clear_date_field_internal(name, value)
+      self.__clear_date_field_internal(name, value)
     elif col_type == ColumnType.NUMBER:
-      self.clear_number_field_internal(name, value)
+      self.__clear_number_field_internal(name, value)
     elif col_type == ColumnType.SELECT:
-      self.clear_select_field_internal(name, value)
+      self.__clear_select_field_internal(name, value)
     elif col_type == ColumnType.MULTI_SELECT:
-      self.clear_multi_select_field_internal(name, value)
+      self.__clear_multi_select_field_internal(name, value)
     elif col_type == ColumnType.FILE:
-      self.clear_file_field_internal(name, value)
+      self.__clear_file_field_internal(name, value)
     else:
       raise NotImplementedError("No implementation yet for type: " + type.name)
 
-  def clear_text_field_internal(self, name: str):
-    self.properties[name]["rich_text"] = []
-    self.commit_required = True
+  def __clear_text_field_internal(self, name: str):
+    self.__properties[name]["rich_text"] = []
+    self.__commit_required = True
 
-  def clear_date_field_internal(self, name: str):
-    self.properties[name]["date"] = None
-    self.commit_required = True
+  def __clear_date_field_internal(self, name: str):
+    self.__properties[name]["date"] = None
+    self.__commit_required = True
 
-  def clear_number_field_internal(self, name: str):
-    self.properties[name]["number"] = None
-    self.commit_required = True
+  def __clear_number_field_internal(self, name: str):
+    self.__properties[name]["number"] = None
+    self.__commit_required = True
 
-  def clear_select_field_internal(self, name: str):
-    self.properties[name]["select"] = None
-    self.commit_required = True
+  def __clear_select_field_internal(self, name: str):
+    self.__properties[name]["select"] = None
+    self.__commit_required = True
 
-  def clear_multi_select_field_internal(self, name: str):
-    self.properties[name]["multi_select"] = []
-    self.commit_required = True
+  def __clear_multi_select_field_internal(self, name: str):
+    self.__properties[name]["multi_select"] = []
+    self.__commit_required = True
 
-  def clear_file_field_internal(self, name: str):
-    self.properties[name]["files"] = []
-    self.commit_required = True
+  def __clear_file_field_internal(self, name: str):
+    self.__properties[name]["files"] = []
+    self.__commit_required = True
