a508fa13bfa45d1335a350b1ce8b6d51b7de5575
Nishant Totla <nishanttotla@gmail.com>
2024-12-03 15:01:21 -0500

    Add basic flask server setup

---
+++ flask_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+  projectpath = request.form['projectFilepath']
+  return "This is cool! " + projectpath
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
