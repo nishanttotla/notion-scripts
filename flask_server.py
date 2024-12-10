00b737552a1968b196e2da54f775445803cb4835
Nishant Totla <nishanttotla@gmail.com>
2024-12-09 17:28:52 -0800

    Rename var

--- flask_server.py
+++ flask_server.py
@@ -14,19 +14,19 @@ def index():
 
 @app.route('/update_result', methods=['POST'])
 def update_result():
-  imdb_id = request.form['imdbId']
+  imdb_ids = request.form["imdbIds"]
   action_log = []
-  if not imdb_id:
+  if not imdb_ids:
     action_log.append("Will not run script with empty IMDB ID.")
   else:
-    pprint("+++++++++++ Starting update run for IMDB ID: " + imdb_id + " at " +
+    pprint("+++++++++++ Starting update run for IMDB ID: " + imdb_ids + " at " +
            str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    updater = UpdateFromTmdb(imdb_ids=[imdb_id])
+    updater = UpdateFromTmdb(imdb_ids=[imdb_ids])
     action_log = updater.update_shows_and_seasons()
   if action_log:
-    action_log.insert(0, "Failed to update IMDB ID: " + imdb_id)
+    action_log.insert(0, "Failed to update IMDB ID: " + imdb_ids)
   else:
-    action_log.insert(0, "Successfully updated IMDB ID: " + imdb_id)
+    action_log.insert(0, "Successfully updated IMDB ID: " + imdb_ids)
   return render_template('update_result.html', result=action_log)
 
 
