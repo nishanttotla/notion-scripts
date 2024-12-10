28a15eabfd1e2ad43d8073a63215e9cfa02a26af
Nishant Totla <nishanttotla@gmail.com>
2024-12-09 17:58:40 -0800

    Script supports multiple input ids

--- flask_server.py
+++ flask_server.py
@@ -14,19 +14,21 @@ def index():
 
 @app.route('/update_result', methods=['POST'])
 def update_result():
-  imdb_ids = request.form["imdbIds"]
+  imdb_ids = request.form["imdbIds"].replace(" ", "")
   action_log = []
   if not imdb_ids:
-    action_log.append("Will not run script with empty IMDB ID.")
+    action_log.append("Cannot import with empty IMDB IDs.")
   else:
-    pprint("+++++++++++ Starting update run for IMDB ID: " + imdb_ids + " at " +
-           str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-    updater = UpdateFromTmdb(imdb_ids=[imdb_ids])
+    split_imdb_ids = imdb_ids.split(",")
+    pprint("+++++++++++ Starting update run for IMDB IDs: " + imdb_ids +
+           " at " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    updater = UpdateFromTmdb(imdb_ids=split_imdb_ids)
     action_log = updater.update_shows_and_seasons()
   if action_log:
-    action_log.insert(0, "Failed to update IMDB ID: " + imdb_ids)
+    action_log.insert(
+        0, "Received some errors while importing IMDB IDs: " + imdb_ids)
   else:
-    action_log.insert(0, "Successfully updated IMDB ID: " + imdb_ids)
+    action_log.insert(0, "Successfully imported IMDB IDs: " + imdb_ids)
   return render_template('update_result.html', result=action_log)
 
 
